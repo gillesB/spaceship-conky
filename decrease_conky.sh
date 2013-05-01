297f707dd6351e82c52a7c91031316cc25650ac5
Gilles Baatz <baatzgilles@gmail.com>
2013-05-01 18:33:41 +0200

    #2 added two bash scripts which raise and decrease conky

---
+++ decrease_conky.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#decrease conky again
+wmctrl -r gbaatz_spaceship_hud_conky -b remove,above
+wmctrl -r gbaatz_spaceship_hud_conky -b add,below
+
---
+++ raise_conky.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#raise conky
+wmctrl -r gbaatz_spaceship_hud_conky -b remove,below
+wmctrl -r gbaatz_spaceship_hud_conky -b add,above
+
+
