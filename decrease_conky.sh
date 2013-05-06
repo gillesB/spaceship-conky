44c5a4126691471a6a256bea17896acbb38c691b
Gilles Baatz <baatzgilles@gmail.com>
2013-05-06 20:44:18 +0200

    removed decrease_conky.sh and raise_conky.sh

--- decrease_conky.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#decrease conky again
-wmctrl -r gillesB_spaceship_hud_conky -b remove,above
-wmctrl -r gillesB_spaceship_hud_conky -b add,below
-
--- raise_conky.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#raise conky
-wmctrl -r gillesB_spaceship_hud_conky -b remove,below
-wmctrl -r gillesB_spaceship_hud_conky -b add,above
-
-
